f0d2f45daf0ea2cf5f2fd3adee897b0dd6217a48
DrOtter3000 <info@darknet-online.com>
2025-06-20 17:15:54 +0200

    fix take intelligence crash

--- intelligence/intelligence.gd
+++ intelligence/intelligence.gd
@@ -4,6 +4,8 @@ extends Area3D
 var interactor_text := "Take Intelligence(LMB)"
 
 
+# Looks weird but works. Crashes if using queue_free() 
 func interact():
 	Gamestate.intelligence_taken = true
-	queue_free()
+	$CollisionShape3D.queue_free()
+	$Briefcase2.visible = false
